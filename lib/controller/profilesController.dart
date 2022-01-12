299db8bb420501f9abdb7aae46446d0b3f23c71b
Pascal Joret <pascal.joret@gmail.com>
2022-01-12 10:43:52 +0100

    Affichage UID sur le profilesController.dart

--- lib/controller/profilesController.dart
+++ lib/controller/profilesController.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_messager_v2/controller/fireBaseController.dart';
 
 class ProfilesController extends StatefulWidget {
   const ProfilesController({Key? key}) : super(key: key);
@@ -8,10 +10,11 @@ class ProfilesController extends StatefulWidget {
 }
 
 class _ProfilesControllerState extends State<ProfilesController> {
+  User? utilisateur = FirebaseController().firebase_auth_instance.currentUser;
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text("Profiles"),
+      child: Text("Profiles uid:${utilisateur!.uid}"),
     );
   }
 }
