b1183c4ef61fa53d4329b0401fcf19204493d49d
pascal <pascal.joret@gmail.com>
2023-02-04 16:01:02 +0100

    Ajout du bouton et la loqigue pour envoyer le message

--- lib/views/zoneDeText.dart
+++ lib/views/zoneDeText.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_messager_v2/controller/fireBaseController.dart';
 import 'package:flutter_messager_v2/model/Utilisateur.dart';
 
 class ZoneDeText extends StatefulWidget {
   Utilisateur tchatUSer;
+  String utilisateur;
 
-  ZoneDeText({required this.tchatUSer});
+  ZoneDeText({required this.tchatUSer, required this.utilisateur});
 
   @override
   ZoneDeTextState createState() => ZoneDeTextState();
 }
 
 class ZoneDeTextState extends State<ZoneDeText> {
+  TextEditingController _textEditingController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -22,13 +26,32 @@ class ZoneDeTextState extends State<ZoneDeText> {
           IconButton(onPressed: () {}, icon: Icon(Icons.photo_library)),
           Flexible(
               child: TextField(
+            controller: _textEditingController,
             decoration:
                 InputDecoration.collapsed(hintText: "Ecrivez un message"),
             maxLines: null, //Permet d'ecrire sur autant de ligne que necessaire
           )),
-          IconButton(onPressed: () {}, icon: Icon(Icons.send))
+          IconButton(
+              onPressed: () {
+                sendMessageOnButtonPressed();
+              },
+              icon: Icon(Icons.send))
         ],
       ),
     );
+  }
+
+  void sendMessageOnButtonPressed() {
+    if (_textEditingController.text.isNotEmpty) {
+      String text = _textEditingController.text;
+      //Envoyer sur firebase
+      FirebaseController()
+          .sendMessages(widget.utilisateur, widget.tchatUSer, text);
+      _textEditingController.clear();
+      FocusScope.of(context).requestFocus(
+          FocusNode()); //Ferme le clavier apres l'envoi du message
+    } else {
+      //TODO Afficher un message d'erreur
+    }
   }
 }
