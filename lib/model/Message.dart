0b53c7ea88a76afa99f8a763d9b1c7e053c97532
pascal <pascal.joret@gmail.com>
2023-02-09 14:44:01 +0100

    Correction de Message.dart

--- lib/model/Message.dart
+++ lib/model/Message.dart
@@ -1,16 +1,20 @@
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter_messager_v2/controller/DateController.dart';
 
 class Message {
   String? from;
   String? to;
   String? message;
-  String? dateString;
+  String? imageUrl;
+  String date = "";
 
   Message({required DataSnapshot snapshot}) {
     Map map = snapshot.value as Map;
-    from = map["From"];
-    to = map["To"];
-    message = map["Message"];
-    dateString = map["time"];
+    from = map["from"];
+    to = map["to"];
+    message = map["message"];
+    String date = map["date"];
+    date = DateController().getDate(date);
+    imageUrl = map["imageUrl"];
   }
 }
